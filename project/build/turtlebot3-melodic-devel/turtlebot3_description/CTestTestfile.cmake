37649c6b385898319da8fe2b91d10ee3e55af4fc
magorors <magorors_kollo@yahoo.com>
2022-01-09 13:01:01 +0200

    Images

--- project/build/turtlebot3-melodic-devel/turtlebot3_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/auto/Magor/University/RCS/project/src/turtlebot3-melodic-devel/turtlebot3_description
-# Build directory: /home/auto/Magor/University/RCS/project/build/turtlebot3-melodic-devel/turtlebot3_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- project/build/turtlebot3-melodic-devel/turtlebot3_teleop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/auto/Magor/University/RCS/project/src/turtlebot3-melodic-devel/turtlebot3_teleop
-# Build directory: /home/auto/Magor/University/RCS/project/build/turtlebot3-melodic-devel/turtlebot3_teleop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
